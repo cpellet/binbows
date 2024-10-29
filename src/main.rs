8c8d4c73be6a2ef4609755d895b2adcffc08f6a7
Cyrus Pellet <cyrus135@hotmail.fr>
2024-10-29 18:54:35 +0100

    First commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something(); 
+    loop {}
+}
